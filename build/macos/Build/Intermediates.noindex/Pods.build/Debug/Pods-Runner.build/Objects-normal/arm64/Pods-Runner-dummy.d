288cba7fbed41aef6f3cbc318a57690d7af7d72b
SHANU-KHANDALA <shanukhandala5@gmail.com>
2025-12-05 01:01:34 +0530

    starting over

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/shanukhandala/Development/projects/routex_testing/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/shanukhandala/Development/projects/routex_testing/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/shanukhandala/Development/projects/routex_testing/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/shanukhandala/Development/projects/routex_testing/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
