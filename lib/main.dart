b699437bfe635dbc268eb3624df2137f3e1974b5
SHANU-KHANDALA <shanukhandala5@gmail.com>
2025-12-14 01:22:05 +0530

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // 1. Added Firebase Core
+import 'firebase_options.dart'; // 2. Added the generated config file
 import 'login_page.dart';
 import 'dashboard_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
